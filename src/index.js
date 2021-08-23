08e2731f9d09d8932568e020baf558427baedb70
michael dobak <mdobak44@icloud.com>
2021-08-23 12:59:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => this.innerText = "Thanks for clicking me, I needed that."} />
   </div>,
   document.getElementById('root')
 );
